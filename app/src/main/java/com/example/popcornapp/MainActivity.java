c71c6ceee664165ecf63f40e00309e796a4b3e02
Maud Genetet <maud.genetet@enseirb-matmeca.fr>
2026-01-03 14:14:17 +0100

    comment gallery and slidehow

--- app/src/main/java/com/example/popcornapp/MainActivity.java
+++ app/src/main/java/com/example/popcornapp/MainActivity.java
@@ -51,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
         // ---- DESTINATIONS ----
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home,
-                R.id.nav_gallery,
-                R.id.nav_slideshow,
+                // R.id.nav_gallery,
+                // R.id.nav_slideshow,
                 R.id.nav_profile)
                 .setOpenableLayout(drawer)
                 .build();
